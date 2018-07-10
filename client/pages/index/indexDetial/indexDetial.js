7771b2f969248faace8ebde1737bf9d0941eaecc
OcaenEyes <1450136519@qq.com>
2018-07-10 23:59:05 +0800

    update
    增加分享点击函数

--- client/pages/index/indexDetial/indexDetial.js
+++ client/pages/index/indexDetial/indexDetial.js
@@ -24,21 +24,21 @@ Page({
     });
     // console.log(currentIndexId);
 
-    // shared内容
-    var indexsShared = wx.getStorageSync('indexs_Shared');
+    // // shared内容
+    // var indexsShared = wx.getStorageSync('indexs_Shared');
 
-    if (indexsShared) {
-      var indexDetialShared = indexsShared[indexDetialId];
-      this.setData({
-        shared: indexDetialShared
-      })
-    } else {
-      var indexsShared = {};
-      indexsShared[indexDetialId] = false;
-      wx.setStorageSync('indexs_Shared', indexsShared)
-    };
+    // if (indexsShared) {
+    //   var indexDetialShared = indexsShared[indexDetialId];
+    //   this.setData({
+    //     shared: indexDetialShared
+    //   })
+    // } else {
+    //   var indexsShared = {};
+    //   indexsShared[indexDetialId] = false;
+    //   wx.setStorageSync('indexs_Shared', indexsShared)
+    // };
 
-// collected内容
+    // collected内容
     var indexsCollected = wx.getStorageSync('indexs_Collected');
 
     if (indexsCollected) {
@@ -72,33 +72,57 @@ Page({
 
     this.setData({
       collected: indexDetialCollected
-    }); 
+    });
     wx.showToast({
+      // 三元表达式，true 收， false取
       title: indexDetialCollected ? '收藏成功' : '取消成功',
       duration: 800,
-      icon: 'success'});
-    // console.log(this.data.collected);
+      icon: 'success'
+    });
+
   },
 
 
 
-  onShareTap: function (event) {
-    var currentIndexId = this.data.currentIndexId;
+  onShareTap: function(event) {
+    // var currentIndexId = this.data.currentIndexId;
 
-    var indexsShared = wx.getStorageSync('indexs_Shared');
-    
-    var indexDetialShared = indexsShared[currentIndexId];
+    // var indexsShared = wx.getStorageSync('indexs_Shared');
 
-    indexDetialShared = !indexDetialShared;
+    // var indexDetialShared = indexsShared[currentIndexId];
 
-    indexsShared[currentIndexId] = indexDetialShared;
-    console.log(indexsShared[currentIndexId]);
-    console.log(indexsShared);
-    wx.setStorageSync('indexs_Shared', indexsShared);
+    // indexDetialShared = !indexDetialShared;
 
-    this.setData({
-      shared: indexDetialShared
-    });
+    // indexsShared[currentIndexId] = indexDetialShared;
+    // console.log(indexsShared[currentIndexId]);
+    // console.log(indexsShared);
+    // wx.setStorageSync('indexs_Shared', indexsShared);
+
+    // this.setData({
+    //   shared: indexDetialShared
+    // });
+
+    var itemList = [
+      '分享给微信好友',
+      '分享到朋友圈',
+      '分享到QQ',
+      '分享到QQ空间',
+      '分享到微博',
+    ]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: '#689845',
+      success: function(res) {
+        // res.cancel             用户是不是点击了取消按钮
+        // res.tapIndex           数组元素的序号， 从0开始
+        wx.showModal({
+          title: '用户' + itemList[res.tapIndex],
+          content: '暂时不支持分享功能,小哥哥正在努力开发中',
+        })
+
+      },
+
+    })
 
 
   },
@@ -128,55 +152,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 })
-
-
-
-
-
-
