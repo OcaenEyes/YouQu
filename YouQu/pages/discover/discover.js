dde3670a644871456a95dd4084dc6fb757bc171b
OcaenEyes <1450136519@qq.com>
2018-07-22 23:19:06 +0800

    update
    update

---
+++ YouQu/pages/discover/discover.js
@@ -0,0 +1,88 @@
+// pages/discover/discover.js
+var app = getApp();
+Page({
+
+  // restful api json
+  // soap xml
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250";
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
+  },
+  getMovieListData: function(url) {
+    wx.request({
+      url: url,
+      data: {},
+      header: {
+        "Content-Type": "application/xml"
+      },
+      method: 'GET',
+      success: function(res) {
+        console.log(res);
+
+      },
+      fail: function() {
+        console.log('failed');
+        console.log(Error);
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
